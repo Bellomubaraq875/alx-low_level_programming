08b510dcd1c62dedaeb50e39f1222251b42b7ef7
Bellomubaraq875 <bellomubaraq875@gmail.com>
2023-03-16 15:51:21 -0700

    secod

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Return: 0 or NULL
+ */
+int main(void)
+{
+	int mul, argc = 0, num1 = 0, num2 = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if ((num1 <= 'z') || (num2 <= 'z'))
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	mul = num1 * num2;
+	printf("%d\n", mul);
+	return (0);
+}
