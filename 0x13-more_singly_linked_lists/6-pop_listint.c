bf9762038027f7b1547ec092492d5d65bad5b429
Bellomubaraq875 <bellomubaraq875@gmail.com>
2023-04-04 16:05:02 -0700

    this function frees list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+* pop_listint - This function deleted the head node of a linked list
+* and returns the head node's data or 0 if the linked list is empty
+* @head: the pointer to the pointer to the head node
+* Return: int n or 0
+*/
+int pop_listint(listint_t **head)
+{
+	listint_t *atm, *new;
+	int a;
+
+	if (head == NULL)
+		return (0);
+	atm = new = *head;
+	if (*head)
+	{
+		a = (atm)->n;
+		*head = atm->next;
+		free(new);
+	}
+	else
+		a = 0;
+	return (a);
+}
